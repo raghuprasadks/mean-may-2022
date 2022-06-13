fcb7881204268b7734d2b65559490c168d0feea0
raghu prasad ks <prasadraghuks@gmail.com>
2022-06-13 10:21:39 +0530

    express

--- express/1-GettingStarted.js
+++ express/1-GettingStarted.js
@@ -1,4 +1,7 @@
 const express = require('express')
+const url = require('url')
+const querystring = require('querystring')
+
 const app = express()
 port = 3000
 
@@ -11,10 +14,33 @@ app.get('/welcome',function(req,res){
     console.log('get method:welcome')
     res.send('Welcome to join me to learn express')
 })
-
+//localhost:3000/registration/raghu
 app.get('/registration/:name',function(req,res){
     yourname = req.params.name
     res.send(yourname +' Thanks for registering with us')
+})
+//localhost:3000/info?name=raghu&city=bengaluru
+app.get('/info',function(req,res){
+    yourname = req.query.name
+    yourcity = req.query.city
+    res.send("your name "+yourname +" your city "+yourcity)
+    
+})
+
+//localhost:3000/querystring
+
+app.get('/querystring',function(req,res){
+
+   
+let rawUrl = 'https://stackabuse.com/?page=2&limit=3';
+
+let parsedUrl = url.parse(rawUrl);
+console.log('protocol '+parsedUrl.protocol)
+console.log('host ' +parsedUrl.host)
+console.log('parsed query '+parsedUrl.query)
+let parsedQs = querystring.parse(parsedUrl.query);
+res.send(parsedQs)
+
 })
 
 
